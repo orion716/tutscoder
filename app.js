547bb513ec75541464a2456c7be759b831703f2b
orion716 <nngochanhtk@gmail.com>
2021-07-10 22:30:32 +0700

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+
+var app = express()
+
+// view engine setup
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+module.exports = app
